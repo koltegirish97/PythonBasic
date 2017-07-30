b65ac201fcbaabe5353472486e8b153d2332f56d
koltegirish97 <koltegirish97@gmail.com>
2017-07-30 11:43:26 +0530

    function_params.py

--- function_params.py
+++ function_params.py
@@ -1,7 +1,9 @@
 
-def print_msg(msg, error="No error",*kwargs):
+#difference between *kwargs and **kwargs
+def print_msg(msg, error="No error", *args, **kwargs):
 	print ("Log :" +error+ ":" +msg )
-	print(kwargs)
+	print("Args :", args)
+	print("kwargs :", kwargs)
 
 
-print_msg("This will be logged","File not found","1","2","32","234")
+print_msg("This will be logged","File not found", "abcd", key= "1,2,32,234",key_2 = "23,45,45,67,677,78")
